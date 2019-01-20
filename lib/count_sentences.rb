7883c485e176422a8db605400e9ac9b04e1bc6a0
ebuchheim <emma.buchheim@gmail.com>
2019-01-20 08:41:44 -0800

    add sentence finding logic

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    !!self.match(/\.$/)
   end
 
   def question?
-
+    !!self.match(/\?$/)
   end
 
   def exclamation?
-
+    !!self.match(/!$/)
   end
 
   def count_sentences
-
+    split_strings = self.split(/[.!?]+/)
+    split_strings.length
   end
 end
